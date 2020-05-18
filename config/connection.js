0c2eb958887818b5eea0e65a807a4bf169ac2e21
aaivey22 <angelawings22@gmail.com>
2020-05-17 20:30:55 -0400

    updates to burgercontroller and connectionjs

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
-var mysql = require("mysql");
+const mysql = require("mysql");
 
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,6 +1,38 @@
-var express = require("express");
-var burger = require("../models/burger");
-var router = express.Router();
+const express = require("express");
+const burger = require("../models/burger");
+const router = express.Router();
 
+// Backend retrieves burger names list to render in index.handlebars
+
+router.get("/", (req, res) => {
+    burger.selectAll((data) => {
+        const burgersData = {
+            burgers: data
+        };
+        console.log(burgersData);
+        res.render("index", burgersData)
+    })
+})
+
+// POST route adds a new burger to the db table and refreshes the page to display the new burger
+router.post("/api/burgers", (req, res) => {
+    console.log(req.body.name)
+    burger.insertOne(req.body.name, (result) => {
+res.redirect("/")
+    });
+});
+
+// PUT route changes the burger obj state
+router.put("/api/burgers/:id", (req, res) => {
+    const dondition = "id = " + req.params.id;
+    
+burger.updateOne(1, condition, (result) => {
+    if (result.changedRows == 0) {
+        return res.status(404).end();
+    } else {
+        res.status(200).end();
+    }
+})
+});
 
 module.exports = router;
