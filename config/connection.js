0812c38f7c3c230768007e7e96635d2f4486912d
aaivey22 <angelawings22@gmail.com>
2020-05-21 11:31:21 -0400

    deploying to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,20 @@
 const mysql = require("mysql");
+const jawsDB = require("./heroku")
+
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "",
+//   database: "burgers_db"
+// });
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "burgers_db"
+  host: jawsDB.host,
+  port: jawsDB.port,
+  user: jawsDB.user,
+  password: jawsDB.password,
+  database: jawsDB.database
 });
 
 connection.connect(function(err) {
