3d68e950a35ec94ea0fdced8066a5409bcef7668
aaivey22 <angelawings22@gmail.com>
2020-05-21 21:41:56 -0400

    corrections to heroku conenction

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,20 @@
 const mysql = require("mysql");
-const jawsDB = require("./heroku")
 
-// const connection = mysql.createConnection({
-//   host: "localhost",
-//   port: 3306,
-//   user: "root",
-//   password: "",
-//   database: "burgers_db"
-// });
+let connection;
 
-const connection = mysql.createConnection({
-  host: "qn66usrj1lwdk1cc.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-  port: 3306,
-  user: "eiww1u37um4hriye",
-  password: "ve00xmv1twxg47cm",
-  database: "pi97p3s8xzlisdax"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "burgers_db"
+  });
+};
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
