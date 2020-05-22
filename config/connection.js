862fa883ecc314daa88f8ed1158fe3214ed80441
aaivey22 <angelawings22@gmail.com>
2020-05-21 22:52:27 -0400

    heroku errors

--- config/connection.js
+++ config/connection.js
@@ -9,8 +9,8 @@ if (process.env.JAWSDB_URL) {
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "",
-    database: "burgers_db"
+    password: "ve00xmv1twxg47cm",
+    database: "pi97p3s8xzlisdax"
   });
 };
 
