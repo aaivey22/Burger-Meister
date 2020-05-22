bd8fc4ec5ac9232587de5b6345020241e42ec554
aaivey22 <angelawings22@gmail.com>
2020-05-21 22:56:11 -0400

    connection issues

--- config/connection.js
+++ config/connection.js
@@ -9,8 +9,8 @@ if (process.env.JAWSDB_URL) {
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "ve00xmv1twxg47cm",
-    database: "pi97p3s8xzlisdax"
+    password: "",
+    database: "burgers_db"
   });
 };
 
