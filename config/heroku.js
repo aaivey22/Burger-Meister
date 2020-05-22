7732e663bafd0e2fe405157de7856e0d4f8c0924
aaivey22 <angelawings22@gmail.com>
2020-05-21 21:48:53 -0400

    heroku issues

--- config/heroku.js
+++
@@ -1,9 +0,0 @@
-const jawsDB = {
-    host: "qn66usrj1lwdk1cc.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    port: 3306,
-    user: "eiww1u37um4hriye",
-    password: "ve00xmv1twxg47cm",
-    database: "pi97p3s8xzlisdax"
-};
-
-module.exports = jawsDB;
